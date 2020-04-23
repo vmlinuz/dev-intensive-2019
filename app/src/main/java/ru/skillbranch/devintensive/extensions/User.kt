0946c30ed235a584f0343d7a2ae984b579f344dd
Volodymyr Salo <volodymyr.salo@gmail.com>
2020-04-23 22:54:15 +0200

    first commit, tasks 0-4 completed.

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/User.kt
@@ -0,0 +1,21 @@
+package ru.skillbranch.devintensive.extensions
+
+import ru.skillbranch.devintensive.models.User
+import ru.skillbranch.devintensive.models.UserView
+import ru.skillbranch.devintensive.utils.Utils
+
+fun User.toUserView() : UserView {
+
+    val nickName = Utils.transliteration("$firstName $lastName")
+    val initials = Utils.toInitials(firstName, lastName)
+    val status = if (lastVisit == null) "Soha se volt" else if (isOnline) "online" else "Utoljára ${lastVisit.humanizeDiff()} volt"
+
+    return UserView(
+        id,
+        fullName = "$firstName $lastName",
+        avatar = avatar,
+        nickName = nickName,
+        initials = initials,
+        status = status
+    )
+}
