baa799f925763732fdd57ecba75c70c44c617375
Volodymyr Salo <volodymyr.salo@gmail.com>
2020-09-20 02:20:46 +0200

    TestInputLayout error done
    Signed-off-by: Volodymyr Salo <volodymyr.salo@gmail.com>

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,27 +1,21 @@
 package ru.skillbranch.devintensive.ui.profile
 
-import android.graphics.Color
 import android.graphics.ColorFilter
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
-import android.view.KeyEvent
 import android.view.View
-import android.view.inputmethod.EditorInfo
 import android.widget.EditText
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
-import ru.skillbranch.devintensive.extensions.hideKeyboard
-import ru.skillbranch.devintensive.extensions.registerKeyboardStateListener
-import ru.skillbranch.devintensive.models.Bender
 import ru.skillbranch.devintensive.models.Profile
 import ru.skillbranch.devintensive.viewmodels.ProfileViewModel
 
@@ -35,6 +29,22 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var viewModel: ProfileViewModel
     var isEditMode = false
     lateinit var viewFields: Map<String, TextView>
+    val allowedRepositoryPrefixes = arrayOf("https://", "www.", "https://www.").map { s -> s + "github.com/" }
+    val forbiddenRepositorySuffixes = arrayOf(
+        "enterprise",
+        "features",
+        "topics",
+        "collections",
+        "trending",
+        "events",
+        "marketplace",
+        "pricing",
+        "nonprofit",
+        "customer-stories",
+        "security",
+        "login",
+        "join"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //TODO set custom Theme this before super and setContentView
@@ -44,12 +54,40 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
         initViews(savedInstanceState)
         initViewModel()
-        Log.d("M_ProfileActivity","onCreate")
+
+        et_repository.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val content = s?.toString()
+                validateRepository(content)
+            }
+        })
+        Log.d("M_ProfileActivity", "onCreate")
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState?.putBoolean(IS_EDIT_MODE, isEditMode)
+    }
+
+    private fun validateRepository(input: String?) {
+        if (input.isNullOrEmpty()
+            || allowedRepositoryPrefixes.any { input.startsWith(it) }
+            && forbiddenRepositorySuffixes.none {
+                input.endsWith(
+                    it
+                )
+            }
+            && !input.split("github.com/")[1].contains('/')
+        ) {
+            wr_repository.error = null
+            wr_repository.isErrorEnabled = false
+            return
+        }
+
+        wr_repository.error = "Невалидный адрес репозитория"
     }
 
     private fun initViewModel() {
@@ -59,7 +97,7 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     private fun updateTheme(mode: Int) {
-        Log.d("M_ProfileActivity","updateTheme")
+        Log.d("M_ProfileActivity", "updateTheme")
         delegate.setLocalNightMode(mode)
     }
 
@@ -86,12 +124,12 @@ class ProfileActivity : AppCompatActivity() {
         isEditMode = savedInstanceState?.getBoolean(IS_EDIT_MODE, false) ?: false
         showCurrentMode(isEditMode)
 
- //       btn_edit.setOnClickListener(object : View.OnClickListener {
- //           override fun onClick(p0: View?) {
- //               isEditMode = !isEditMode
- //               showCurrentMode(isEditMode)
- //           }
- //       })
+        //       btn_edit.setOnClickListener(object : View.OnClickListener {
+        //           override fun onClick(p0: View?) {
+        //               isEditMode = !isEditMode
+        //               showCurrentMode(isEditMode)
+        //           }
+        //       })
 
         btn_edit.setOnClickListener {
             if (isEditMode) saveProfileInfo()
@@ -117,7 +155,7 @@ class ProfileActivity : AppCompatActivity() {
         ic_eye.visibility = if (isEdit) View.GONE else View.VISIBLE
         wr_about.isCounterEnabled = isEdit
 
-        with (btn_edit) {
+        with(btn_edit) {
             val filter: ColorFilter? = if (isEdit) {
                 PorterDuffColorFilter(
                     resources.getColor(R.color.color_accent, theme),
@@ -143,9 +181,11 @@ class ProfileActivity : AppCompatActivity() {
             firstName = et_first_name.text.toString(),
             lastName = et_last_name.text.toString(),
             about = et_about.text.toString(),
-            repository = et_repository.text.toString()
+            repository = if (wr_repository.error == null) et_repository.text.toString() else ""
         ).apply {
             viewModel.saveProfileData(this)
+            wr_repository.error = null
+            wr_repository.isErrorEnabled = false
         }
     }
 }
